4d3725c9d26be7a3a34e6558ae7e08318d631eb1
Oscar Castillo <oscar00129@gmail.com>
2023-05-12 18:23:20 -0600

    Power, clear and number functions added.

--- javascript.js
+++ javascript.js
@@ -1,13 +1,38 @@
 const app = Vue.createApp({
     data() {
         return {
-            displayNumber: "0.",
+            displayNumber: "0",
             sound: true
+        }
+    },
+    computed: {
+        isOn(){
+            return this.displayNumber != ""
         }
     },
     methods: {
         numberPressed(number){
+            if(!this.isOn) return;
+            if(this.displayNumber == "0"){
+                this.displayNumber = String(number);
+            }else{
+                this.displayNumber = this.displayNumber + String(number);
+            }
             console.log(number);
+        },
+        CEPressed(){
+            if(!this.isOn) return;
+            this.displayNumber = "0";
+        },
+        OFFPressed(){
+            if(this.isOn) this.displayNumber = ""
+        },
+        ONPressed(){
+            if(!this.isOn) this.displayNumber = "0"
+        },
+        plusMinusPressed(){
+            if(!this.isOn || this.displayNumber == "0") return;
+            this.displayNumber = String(-1 * Number(this.displayNumber))
         }
     }
 })
