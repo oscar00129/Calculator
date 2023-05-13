a052b5c136cd509fba4fcc587e69ec9b39ebd5bf
Oscar Castillo <oscar00129@gmail.com>
2023-05-12 21:02:00 -0600

    Plus operation in normal route (number > operation > number > equal) done.

--- javascript.js
+++ javascript.js
@@ -2,7 +2,11 @@ const app = Vue.createApp({
     data() {
         return {
             displayNumber: "0",
-            sound: true
+            sound: true,
+            operation: null,
+            firstNumber: null,
+            secondNumber: null,
+            clearDisplay: false
         }
     },
     computed: {
@@ -11,10 +15,11 @@ const app = Vue.createApp({
         }
     },
     methods: {
-        numberPressed(number){
+        btnNumberPressed(number){
             if(!this.isOn) return;
-            if(this.displayNumber == "0"){
+            if(this.displayNumber == "0" || this.clearDisplay){
                 this.displayNumber = String(number);
+                this.clearDisplay = false;
             }else{
                 this.displayNumber = this.displayNumber + String(number);
             }
@@ -23,6 +28,7 @@ const app = Vue.createApp({
         CEPressed(){
             if(!this.isOn) return;
             this.displayNumber = "0";
+            this.resetVariables();
         },
         OFFPressed(){
             if(this.isOn) this.displayNumber = ""
@@ -33,6 +39,44 @@ const app = Vue.createApp({
         plusMinusPressed(){
             if(!this.isOn || this.displayNumber == "0") return;
             this.displayNumber = String(-1 * Number(this.displayNumber))
+        },
+        btnPlusPressed(){
+            this.operation = "plus"
+            this.firstNumber = Number(this.displayNumber)
+            this.clearDisplay = true
+        },
+        btnMinusPressed(){
+            this.operation = "minus"
+        },
+        btnMultiplyPressed(){
+            this.operation = "multiply"
+        },
+        btnDividePressed(){
+            this.operation = "divide"
+        },
+        btnPercentagePressed(){
+            this.operation = "percentage"
+        },
+        btnSquareRootPressed(){
+            this.operation = "square_root"
+        },
+        btnMusicPressed(){},
+        btnDotPressed(){},
+        btnEqualPressed(){
+            this.secondNumber = Number(this.displayNumber);
+
+            switch(this.operation){
+                case "plus":
+                    this.displayNumber = String(this.firstNumber + this.secondNumber);
+            }
+
+            this.clearDisplay = true;
+            this.resetVariables();
+        },
+        resetVariables(){
+            this.firstNumber = null;
+            this.secondNumber = null;
+            this.operation = null;
         }
     }
 })
